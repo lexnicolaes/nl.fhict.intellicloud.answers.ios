e0c54c8158a1d0e71620bcb05711376178ef5274
Joris Vervuurt <jorisvervuurt@MacBook-Pro-van-Joris-Vervuurt.local>
2013-11-20 14:13:28 +0100

    Initial commit.
    Empty application, including the required localization files.

---
+++ Answers/Answers/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Answers
+//
+//  Created by Joris Vervuurt on 20-11-13.
+//  Copyright (c) 2013 IntelliCloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Answers/Answers/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Answers
+//
+//  Created by Joris Vervuurt on 20-11-13.
+//  Copyright (c) 2013 IntelliCloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
