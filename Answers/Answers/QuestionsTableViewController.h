e27db746b70299e78afd96051351ef34b7f50d43
Lex Nicolaes <lex.nicolaes@gmail.com>
2013-11-21 12:09:43 +0100

    Added tableview

---
+++ Answers/Answers/QuestionsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuestionsTableViewController.h
+//  Answers
+//
+//  Created by Lex Nicolaes on 21-11-13.
+//  Copyright (c) 2013 IntelliCloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuestionsTableViewController : UITableViewController
+
+@end
