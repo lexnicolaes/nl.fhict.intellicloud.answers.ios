272c7773fcad7495b2ec15aa8a42d87b040a4728
paulpoos <paul_poos5@hotmail.com>
2013-12-09 12:07:12 +0100

    AboutTableViewController added AcknowledgementViewController added

---
+++ Answers/Answers/AboutTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  AboutTableViewController.h
+//  Answers
+//
+//  Created by Paul Poos on 05-12-13.
+//  Copyright (c) 2013 IntelliCloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BaseViewController.h"
+
+@interface AboutTableViewController : UITableViewController
+
+@end
